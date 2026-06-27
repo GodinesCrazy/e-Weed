badef00dfaae428834103c8d840099c8ea0425ae
GodinesCrazy <165341377+GodinesCrazy@users.noreply.github.com>
2026-06-27 17:28:25 -0400

    Add timed actuator control API

--- controller_uno/src/actuators.h
+++ controller_uno/src/actuators.h
@@ -6,12 +6,15 @@
 namespace Actuators {
 
 void begin();
+void tick(const SensorData &sensors);
+
 bool relayLevelOn();
 bool relayLevelOff();
 
-/** Escribe hardware y estado lógico; aplica interlocks con niveles actuales. */
 bool setRelay(RelayId id, bool on, const SensorData &sensors);
+bool pulseRelay(RelayId id, uint32_t durationMs, const SensorData &sensors);
 bool getRelay(RelayId id);
+
 void allOutputsOff(const SensorData &sensors);
 void disableCriticalOutputs(const SensorData &sensors);
 
